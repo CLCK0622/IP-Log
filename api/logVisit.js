977c24270395ac2c752eb4df57aa305232578f7a
CLCK0622 <zhongyi070622@163.com>
2024-10-27 23:00:48 +0800

    allow cors

--- api/logVisit.js
+++ api/logVisit.js
@@ -18,6 +18,10 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
     try {
         const { fullUrl } = req.body;
         const ip = req.headers['x-forwarded-for'] || "N/A";
