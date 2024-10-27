3ae511cd23d579700ec27a7fd99c193199fdfe8d
CLCK0622 <zhongyi070622@163.com>
2024-10-27 23:26:01 +0800

    add country icon

--- api/logVisit.js
+++ api/logVisit.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { fullUrl } = JSON.parse(req.body);
+    const { fullUrl } = req.body;
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const siteUrl = fullUrl || req.headers['referer'] || "N/A";
 
